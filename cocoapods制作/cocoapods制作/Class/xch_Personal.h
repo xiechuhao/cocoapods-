014a451ba0fdef6f7c51254291d470d35b9d69bd
解楚豪 <1025745401@qq.com>
2017-06-07 17:30:46 +0800

    添加class文件

---
+++ cocoapods制作/cocoapods制作/Class/xch_Personal.h
@@ -0,0 +1,13 @@
+//
+//  xch_Personal.h
+//  cocoapods制作
+//
+//  Created by risenb_mac_002 on 17/6/7.
+//  Copyright © 2017年 risenb_mac_002. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface xch_Personal : NSObject
+
+@end
---
+++ cocoapods制作/cocoapods制作/Class/xch_Personal.m
@@ -0,0 +1,13 @@
+//
+//  xch_Personal.m
+//  cocoapods制作
+//
+//  Created by risenb_mac_002 on 17/6/7.
+//  Copyright © 2017年 risenb_mac_002. All rights reserved.
+//
+
+#import "xch_Personal.h"
+
+@implementation xch_Personal
+
+@end
